6ff534589c197c71ef2f28c242e61c7615b982d7
Atsuhiro <atcheri@gmail.com>
2024-11-11 10:06:57 +0100

    feat: add a bit of styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen items-center justify-center bg-purple-50 p-4">
+	<div class="w-full max-w-md overflow-hidden rounded-xl bg-white shadow-lg">
+		<div class="flex items-center bg-purple-500 p-4">
+			<!-- <Menu className="text-white h-6 w-6" /> -->
+			<h1 class="ml-4 text-xl font-medium text-white">Todo List</h1>
+		</div>
+		<div class="p-4">
+			{@render children()}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
-	let todos = $state([
-		{ id: window.crypto.randomUUID(), text: 'Todo 1', done: false },
-		{ id: window.crypto.randomUUID(), text: 'Todo 2', done: false }
+	type Todo = {
+		id: string;
+		text: string;
+		done: boolean;
+	};
+	type Filter = 'all' | 'active' | 'completed';
+
+	let todos = $state<Todo[]>([
+		{ id: '1', text: 'Todo 1', done: false },
+		{ id: '2', text: 'Todo 2', done: false }
 	]);
+
+	let filter = $state<Filter>('all');
+
+	$effect(() => {
+		const savedTodos = localStorage.getItem('todos');
+		if (savedTodos) {
+			todos = JSON.parse(savedTodos);
+		}
+	});
+
+	$effect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	});
+
+	const filterTodos = (todos: Todo[]) => {
+		if (filter === 'all') {
+			return todos;
+		}
+		if (filter === 'active') {
+			return todos.filter((todo) => !todo.done);
+		}
+		if (filter === 'completed') {
+			return todos.filter((todo) => todo.done);
+		}
+		return [];
+	};
+
+	let filteredTodos = $derived(filterTodos(todos));
 
 	const addTodo = (event: KeyboardEvent) => {
 		if (event.key !== 'Enter') {
@@ -33,18 +68,34 @@
 		}
 		todos[index].done = !todos[index].done;
 	};
+
+	const setFilter = (newFilter: Filter) => {
+		filter = newFilter;
+	};
+
+	const remaining = () => {
+		return todos.filter((todo) => !todo.done).length;
+	};
 </script>
 
 <input onkeydown={addTodo} type="text" placeholder="add a todo" />
 
 <div class="todos">
-	{#each todos as todo, i}
-		<div class="todo">
+	{#each filteredTodos as todo, i}
+		<div class="todo" class:completed={todo.done}>
 			<input type="text" value={todo.text} oninput={editTodo} data-index={i} />
 			<input type="checkbox" checked={todo.done} onchange={toggleTodo} data-index={i} />
 		</div>
 	{/each}
 </div>
+
+<div class="filters">
+	<button onclick={() => setFilter('all')}>All</button>
+	<button onclick={() => setFilter('active')}>Active</button>
+	<button onclick={() => setFilter('completed')}>Completed</button>
+</div>
+
+<p>{remaining()} Remaining</p>
 
 <style>
 	.todos {
@@ -55,6 +106,11 @@
 
 	.todo {
 		position: relative;
+		transition: opacity 0.3s;
+	}
+
+	.completed {
+		opacity: 0.4;
 	}
 
 	input[type='text'] {
@@ -67,5 +123,9 @@
 		right: 4%;
 		top: 50%;
 		translate: 0% -50%;
+	}
+
+	.filters {
+		margin-block: 1rem;
 	}
 </style>
